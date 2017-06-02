60e673bfe7996163138f90ea4553601198286c98
hi <hi>
2017-06-02 18:05:18 +0800

    Fix the data loading path

--- src/assets/heatmap.js
+++ src/assets/heatmap.js
@@ -10,7 +10,7 @@ window.onload = function () {
         colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"], // alternatively colorbrewer.YlGnBu[9]
         days = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
         times = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"];
-        datasets = ["http://smartapi-dev.openweb.solutions/public/data.tsv"];
+        datasets = ["/public/data.tsv"];
 
    
     function getDayLabels (svg) {
