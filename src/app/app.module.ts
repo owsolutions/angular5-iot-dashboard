28926ab55f91064157e1d4fb086b84c543e1e62a
hi <hi>
2017-06-03 17:42:24 +0800

    Adding router and summary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
+import { SummaryComponent } from './summary/summary.component';
+
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+
+const appRoutes: Routes = [
+  {
+    path: 'index',
+    component: IndexComponent,
+    data: { title: 'Index page' }
+  },
+  {
+    path: 'summary',
+    component: SummaryComponent,
+    data: {}
+  },
+  { path: '',
+    redirectTo: '/index',
+    pathMatch: 'full'
+  }
+];
+
+
 
 import {
   MdButtonModule, 
@@ -21,9 +44,11 @@ import {
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent
+    IndexComponent,
+    SummaryComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
