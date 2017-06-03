27ce0ea59d8a2e7976aa0a37a27726f1e6e1a533
hi <hi>
2017-06-03 19:22:28 +0800

    Update

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var fetch: Function;
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SummaryComponent implements OnInit {
 
-  constructor() { }
+  summary: {
+    mintemp: Number,
+    maxtemp: Number,
+    minhumidity: Number,
+    maxhumidity: Number
+  };
+  constructor() { 
+    this.summary = {
+      mintemp: 0,
+      minhumidity: 0,
+      maxhumidity: 0,
+      maxtemp:0
+    };
+  }
 
   ngOnInit() {
+
+    fetch('http://smart.openweb.solutions/api/summary')
+    .then(response => response.json())
+    .then(data => {
+      this.summary = data.result[0];
+    });
   }
 
 }
