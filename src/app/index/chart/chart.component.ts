a85bb1d1e7ca7b90963838bdab059ba5832a4ada
hi <hi>
2017-06-11 15:06:22 +0800

    Add chart module

---
+++ src/app/index/chart/chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chart',
+  templateUrl: './chart.component.html',
+  styleUrls: ['./chart.component.scss']
+})
+export class ChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
