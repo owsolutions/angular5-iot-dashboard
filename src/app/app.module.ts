f70affc8c4a8e08b70b25355669fcf9fc593fe86
hi <hi>
2017-06-18 13:51:34 +0800

    Moved routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
@@ -35,45 +34,8 @@ if (IO) {
 
 }
 
-const appRoutes: Routes = [
-  {
-    path: 'index',
-    component: IndexComponent,
-    data: { title: 'Index page' }
-  },
-  {
-    path: 'summary',
-    component: SummaryComponent,
-    data: {}
-  },
-  {
-    path: 'settings',
-    component: SettingsComponent,
-    data: {}
-  },
-  {
-    path: 'devices',
-    component: DevicesComponent,
-    data: {}
-  },
-  { path: '',
-    redirectTo: '/index',
-    pathMatch: 'full'
-  }
-];
+import { appRoutesGenerator } from './routes';
 
-
-
-import {
-  MdButtonModule,
-  MdCheckboxModule,
-  MdSidenav,
-  MdToolbarModule,
-  MdSidenavContainer,
-  MdSidenavModule,
-  MdNavListCssMatStyler,
-  MdListItem
-} from '@angular/material';
 import { QuickTimeComponent } from './master/quick-status/quick-time/quick-time.component';
 
 @NgModule({
@@ -90,15 +52,11 @@ import { QuickTimeComponent } from './master/quick-status/quick-time/quick-time.
     QuickTimeComponent
   ],
   imports: [
-    RouterModule.forRoot(appRoutes),
+    appRoutesGenerator(),
     BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
-    HttpModule,
-    MdButtonModule,
-    MdCheckboxModule,
-    MdToolbarModule,
-    MdSidenavModule
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes.ts
@@ -0,0 +1,36 @@
+import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { SummaryComponent } from './summary/summary.component';
+import { SettingsComponent } from './settings/settings.component';
+import { DevicesComponent } from './devices/devices.component';
+
+export const appRoutes: Routes = [
+    {
+        path: 'index',
+        component: IndexComponent,
+        data: { title: 'Index page' }
+    },
+    {
+        path: 'summary',
+        component: SummaryComponent,
+        data: {}
+    },
+    {
+        path: 'settings',
+        component: SettingsComponent,
+        data: {}
+    },
+    {
+        path: 'devices',
+        component: DevicesComponent,
+        data: {}
+    },
+    { path: '',
+        redirectTo: '/index',
+        pathMatch: 'full'
+    }
+];
+
+export function appRoutesGenerator () {
+    return RouterModule.forRoot(appRoutes);
+}
