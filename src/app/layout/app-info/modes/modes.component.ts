1aa2afc8f4c395a864005ed3cc297564c285feb4
yousef <yousef.sami19@gmail.com>
2017-07-05 21:34:37 +0430

    Create thermostat and mode component in app info with some changes in dashboard layout

---
+++ src/app/layout/app-info/modes/modes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modes',
+  templateUrl: './modes.component.html',
+  styleUrls: ['./modes.component.scss']
+})
+export class ModesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
