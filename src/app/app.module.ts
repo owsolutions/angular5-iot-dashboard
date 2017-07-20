f39ceef0c733454767c571bdb4ea53bb2d34884a
yousef <yousef.sami19@gmail.com>
2017-07-20 14:48:56 +0430

    Create new Timeline Component and fixed some issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,6 +32,7 @@ import { FullWidgetComponent } from './elements/widgets/full-widget/full-widget.
 import { ActivityComponent } from './activity/activity.component';
 import { ThermostatComponent } from './layout/app-info/thermostat/thermostat.component';
 import { ModesComponent } from './layout/app-info/modes/modes.component';
+import { ReminderTimelineComponent } from './layout/app-info/reminder-timeline/reminder-timeline.component';
 
 @NgModule({
   declarations: [
@@ -60,7 +61,8 @@ import { ModesComponent } from './layout/app-info/modes/modes.component';
     FullWidgetComponent,
     ActivityComponent,
     ThermostatComponent,
-    ModesComponent
+    ModesComponent,
+    ReminderTimelineComponent
   ],
   imports: [
     appRoutesGenerator(),
