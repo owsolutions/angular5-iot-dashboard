5d50647eabe1cdc62b1fc4786ead4786704e5553
Yousef Sami <yousef.sami19@gmail.com>
2017-07-26 19:41:39 +0430

    Add output-pin-view for widgets inside location row for index/location page (#57)(issue:49)
    * Location row widget with simple style
    
    * OutputPinView component added
    
    * Update the object
    
    * Update the code to use app-output-view
    
    * Update precent
    
    * Checking pin and value

--- src/app/locations/locations.mock.ts
+++ src/app/locations/locations.mock.ts
@@ -5,11 +5,11 @@ export function generateMock (): Array<ILocation> {
 
     function createLocations (id: Number = 3): Array<ILocation> {
         const locations = [
-            {id: 1, name: 'Kitchen' , 'icon': 'four-cooking-accessories-set-for-kitchen.svg'},
-            {id: 2, name: 'Bathroom' , 'icon': 'bathtub.svg'},
-            {id: 3, name: 'Master bedrrom', 'icon': 'fireplace.svg'},
-            {id: 4, name: 'Living room', 'icon': 'living-room.svg'},
-            {id: 5, name: 'Conference room', 'icon': 'conference.svg'}
+            {id: 1, name: 'Kitchen' , 'icon': 'kitchen.svg'},
+            {id: 2, name: 'Bathroom' , 'icon': 'bathroom.svg'},
+            {id: 3, name: 'Master bedrrom', 'icon': 'master-bedroom.svg'},
+            {id: 4, name: 'Kitchen', 'icon': 'kitchen.svg'},
+            {id: 5, name: 'Bathroom', 'icon': 'master-bedroom.svg'}
         ];
         return locations;
     }
