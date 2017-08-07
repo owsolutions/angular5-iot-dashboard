8795a91e9ea17385e0ef6528b61b3e8b0bab7e3c
hi <hi>
2017-08-07 21:27:45 +0800

    Add IResponse

--- src/app/shared/Definitions.ts
+++ src/app/shared/Definitions.ts
@@ -107,3 +107,40 @@ export interface IActivity {
   time: string;
 }
 
+export interface IResponse {
+  apiVersion?: string;
+  context?: string;
+  id?: string;
+  params?: {
+    id?: string
+  };
+  data?: {
+    kind?: string;
+    fields?: string;
+    etag?: string;
+    id?: string;
+    lang?: string;
+    updated?: string;
+    deleted?: boolean;
+    currentItemCount?: Number;
+    itemsPerPage?: Number;
+    startIndex?: Number;
+    totalItems?: Number;
+    pageIndex?: Number;
+    totalPages?: Number;
+    items?: Array<any>
+  };
+  error?: {
+    code?: Number;
+    message: string;
+    errors?: Array<{
+      domain?: string;
+      reason?: string;
+      message?: string;
+      location?: string;
+      locationType?: string;
+      extendedHelp?: string;
+      sendReport?: string;
+    }>
+  };
+}
