a0d0ce2b07ae5329015587dfadb4b2d3e703c20a
hi <hi>
2017-08-10 01:32:01 +0800

    Update!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,26 +10,8 @@ import { appReducersGenerator } from '@app/core/app.reducers';
 
 import { AppComponent } from '@app/app.component';
 
-// Iot related
-import { DevicesComponent } from '@app/iot/devices/devices.component';
-import { WidgetsComponent } from '@app/iot/widgets/widgets.component';
-import { ActivityWidgetComponent } from '@app/iot/activity/activity-widget/activity-widget.component';
-import { LocationsComponent } from '@app/iot/locations/locations.component';
-import { ActivityComponent } from '@app/iot/activity/activity.component';
-import { LocationRowComponent } from '@app/iot/locations/location-row/location-row.component';
-import { LocationEditComponent } from '@app/iot/locations/location-edit/location-edit.component';
-import { DeviceSvgComponent } from '@app/iot/shared/device-svg/device-svg.component';
-import { HelperBoxComponent } from '@app/iot/shared/helper-box/helper-box.component';
-import { OutputPinViewComponent } from '@app/iot/shared/output-pin-view/output-pin-view.component';
-import { DataTableComponent } from '@app/core/data-table/data-table.component';
-import { SwitchWidgetsComponent } from '@app/iot/shared/switch-widgets/switch-widgets.component';
-import { InputPinComponent } from '@app/iot/shared/input-pin/input-pin.component';
-import { OutputPinComponent } from '@app/iot/shared/output-pin/output-pin.component';
-import { IconWidgetsComponent } from '@app/iot/index/icon-widgets/icon-widgets.component';
-import { PlacesComponent } from '@app/iot/index/places/places.component';
-import { IndexComponent } from '@app/iot/index/index.component';
-import { ChartComponent } from '@app/iot/index/chart/chart.component';
 
+import IotModules from '@app/iot/app.module.iot';
 
 import { SettingsComponent } from '@app/core/settings/settings.component';
 import { QuickStatusComponent } from '@app/layout/quick-status/quick-status.component';
@@ -66,55 +48,36 @@ import { MocksService } from '@app/services/mocks.service';
 import { ActionsService } from '@app/services/actions.service';
 import { UserService, AuthGuard } from '@app/services/user.service';
 import { CommunicateService } from '@app/services/communicate.service';
-import { DefaultLayoutComponent } from '@app/default-layout/default-layout.component';
 import { LogoutComponent } from '@app/core/users/logout/logout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent,
     SettingsComponent,
-    ChartComponent,
     QuickStatusComponent,
     QuickChartComponent,
-    SwitchWidgetsComponent,
-    DevicesComponent,
     UserWidgetComponent,
-    ActivityWidgetComponent,
     NavigationComponent,
     AppInfoComponent,
-    IconWidgetsComponent,
-    PlacesComponent,
-    InputPinComponent,
-    OutputPinComponent,
-    LocationsComponent,
     LargeIconWidgetComponent,
     MediumIconWidgetComponent,
     LargeValueWidgetComponent,
     FullWidgetComponent,
-    ActivityComponent,
     ThermostatComponent,
     ModesComponent,
     ReminderTimelineComponent,
     PageHeaderComponent,
     TextInputComponent,
     RadioInputComponent,
-    DeviceSvgComponent,
-    WidgetsComponent,
     SelectInputComponent,
-    HelperBoxComponent,
-    LocationRowComponent,
-    OutputPinViewComponent,
-    LocationEditComponent,
-    DataTableComponent,
     FormElementsComponent,
     UsersComponent,
     RolesComponent,
     UserSingleComponent,
     CheckboxInputComponent,
     LoginComponent,
-    DefaultLayoutComponent,
-    LogoutComponent
+    LogoutComponent,
+    ... IotModules
   ],
   imports: [
     appRoutesGenerator(),
---
+++ src/app/iot/app.module.iot.ts
@@ -0,0 +1,42 @@
+// Iot related
+import { DevicesComponent } from '@app/iot/devices/devices.component';
+import { WidgetsComponent } from '@app/iot/widgets/widgets.component';
+import { ActivityWidgetComponent } from '@app/iot/activity/activity-widget/activity-widget.component';
+import { LocationsComponent } from '@app/iot/locations/locations.component';
+import { ActivityComponent } from '@app/iot/activity/activity.component';
+import { LocationRowComponent } from '@app/iot/locations/location-row/location-row.component';
+import { LocationEditComponent } from '@app/iot/locations/location-edit/location-edit.component';
+import { DeviceSvgComponent } from '@app/iot/shared/device-svg/device-svg.component';
+import { HelperBoxComponent } from '@app/iot/shared/helper-box/helper-box.component';
+import { OutputPinViewComponent } from '@app/iot/shared/output-pin-view/output-pin-view.component';
+import { DataTableComponent } from '@app/core/data-table/data-table.component';
+import { SwitchWidgetsComponent } from '@app/iot/shared/switch-widgets/switch-widgets.component';
+import { InputPinComponent } from '@app/iot/shared/input-pin/input-pin.component';
+import { OutputPinComponent } from '@app/iot/shared/output-pin/output-pin.component';
+import { IconWidgetsComponent } from '@app/iot/index/icon-widgets/icon-widgets.component';
+import { PlacesComponent } from '@app/iot/index/places/places.component';
+import { IndexComponent } from '@app/iot/index/index.component';
+import { ChartComponent } from '@app/iot/index/chart/chart.component';
+import { DefaultLayoutComponent } from '@app/iot/default-layout/default-layout.component';
+
+export default [
+    DevicesComponent,
+    WidgetsComponent,
+    ActivityWidgetComponent,
+    LocationsComponent,
+    ActivityComponent,
+    LocationRowComponent,
+    LocationEditComponent,
+    DeviceSvgComponent,
+    HelperBoxComponent,
+    OutputPinViewComponent,
+    DataTableComponent,
+    SwitchWidgetsComponent,
+    InputPinComponent,
+    OutputPinComponent,
+    IconWidgetsComponent,
+    PlacesComponent,
+    IndexComponent,
+    ChartComponent,
+    DefaultLayoutComponent
+];
