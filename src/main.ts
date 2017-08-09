bc0486345e103e24325b8ae5c45681dcf03f7519
hi <hi>
2017-08-10 02:43:18 +0800

    Use angular cli to load datatable

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
 import { hmrBootstrap } from './hmr';
 
 if (environment.production) {
   enableProdMode();
 }
-
-
 const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
 
 if (environment.hmr) {
