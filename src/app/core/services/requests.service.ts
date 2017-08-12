1419ea3729bdebc2e8e295c5af6068c14389424b
hi <hi>
2017-08-12 18:54:22 +0800

    Add an intentionaly delay for signing user

--- src/app/core/services/requests.service.ts
+++ src/app/core/services/requests.service.ts
@@ -5,6 +5,12 @@ import { PermissionsService } from './permissions.service';
 import { sample, times} from 'lodash';
 import { MocksService } from './mocks.service';
 
+function timeout (ms = 1000): Promise<any> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve() , ms);
+  });
+}
+
 
 @Injectable()
 export class RequestsService {
@@ -16,6 +22,7 @@ export class RequestsService {
   }
 
   async createVPC (vpcInformation: IVPCInformation): Promise<IResponse> {
+    await timeout (2000);
     return this.mocks.createVPC(vpcInformation);
   }
   async getLocations(): Promise<Array<ILocation>> {
