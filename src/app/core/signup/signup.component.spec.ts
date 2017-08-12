f7d407d11532a781c5f7fbc6e74345529dd8b574
hi <hi>
2017-08-12 20:04:59 +0800

    Add testing module

--- src/app/core/signup/signup.component.spec.ts
+++ src/app/core/signup/signup.component.spec.ts
@@ -3,6 +3,8 @@ import { RequestsService } from '@app/core/services/requests.service';
 import { PermissionsService } from '@app/core/services/permissions.service';
 import { MocksService } from '@app/core/services/mocks.service';
 import { SignupComponent } from './signup.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { RouterModule } from '@angular/router';
 
 
 describe('SignupComponent', () => {
@@ -16,6 +18,10 @@ describe('SignupComponent', () => {
         RequestsService,
         MocksService,
         PermissionsService
+      ],
+      imports: [
+        RouterTestingModule,
+        RouterModule
       ]
     })
     .compileComponents();
