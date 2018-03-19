bb2acf1ec5d66d7399dd1c545383acb8d91b0f06
Ali <connexion.founder@outlook.com>
2018-03-19 07:35:55 +0700

    Update the statistics

--- src/app/shared/statistics/statistics.component.ts
+++ src/app/shared/statistics/statistics.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '@app/shared/iot/definitions';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/combineLatest';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.scss']
 })
-export class StatisticsComponent implements OnInit {
+export class StatisticsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private ref: Subscription = null;
+  @Input('statistics') public statistics: Array<{title: string, value: any}> = [];
+  constructor(
+    private store: Store<AppState>,
+  ) { }
 
   ngOnInit() {
+    this.InitDefaultStatistics();
+  }
+
+  private InitDefaultStatistics () {
+    if (this.statistics.length) {
+      return;
+    }
+    this.ref = Observable.combineLatest(
+      this.store.select('locations'),
+      this.store.select('devices'),
+    ).subscribe((sink) => {
+      this.statistics = [];
+      this.statistics.push({
+        title: 'Devices',
+        value: sink[1].length
+      });
+      this.statistics.push({
+        title: 'Locations',
+        value: sink[0].length
+      });
+    });
+  }
+
+  ngOnDestroy () {
+    if (this.ref) {
+      this.ref.unsubscribe();
+    }
   }
 
 }
