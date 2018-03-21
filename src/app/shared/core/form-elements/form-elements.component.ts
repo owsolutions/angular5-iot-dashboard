eef4d0704b94b57fbb925f46e645f8494b5d94fc
Ali <connexion.founder@outlook.com>
2018-03-22 00:21:10 +0700

    Working a code

--- src/app/shared/core/form-elements/form-elements.component.ts
+++ src/app/shared/core/form-elements/form-elements.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { times, random } from 'lodash';
 
 @Component({
   selector: 'app-form-elements',
@@ -7,9 +8,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormElementsComponent implements OnInit {
 
+  public series = [
+    {
+      name: 'Power',
+      type: 'area',
+      data: getMock(),
+      yAxis: 1,
+      gapSize: 5,
+      tooltip: {
+        valueSuffix: ' Wh',
+      },
+      marker: {
+        enabled: false,
+        fillColor: '#FFF',
+        lineColor: '#52dba7',
+        lineWidth: 1,
+        symbol: 'circle',
+        radius: 3
+      },
+      fillColor: 'rgba(220,237,200,0.4)',
+      lineColor: 'rgba(220,237,200,0.9)'
+    },
+    {
+      name: 'Consumption',
+      type: 'area',
+      gapSize: 5,
+      data: getMock(),
+      marker: {
+        enabled: false,
+        fillColor: '#FFF',
+        lineColor: '#2bc661',
+        lineWidth: 1,
+        symbol: 'circle',
+        radius: 3
+      },
+      tooltip: {
+        valueSuffix: ' kWh'
+      },
+      fillColor: 'rgb(156,204,101, 0.4)',
+      lineColor: 'rgb(156,204,101, 0.9)'
+    },
+    {
+      name: 'Water',
+      type: 'area',
+      data: getMock(),
+      gapSize: 5,
+      yAxis: 2,
+      tooltip: {
+        valueDecimals: 2,
+        valueSuffix: ' L'
+      },
+      marker: {
+        enabled: false,
+        fillColor: '#FFF',
+        lineColor: 'rgb(0, 212, 252)',
+        lineWidth: 1,
+        symbol: 'circle',
+        radius: 3
+      },
+      fillColor: 'rgb(124,179,66, 0.6)',
+      lineColor: 'rgb(124,179,66, 0.9)',
+      threshold: null
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
+}
 
+function getMock (count = 50) {
+  return times(count, (index) => {
+    return [
+      1317888000000 + (index * 60000),
+      random(32000, 40000) / 100,
+      random(32000, 40000) / 100,
+      random(32000, 40000) / 100,
+    ];
+  });
 }
