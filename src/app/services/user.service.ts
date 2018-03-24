b6f2f16a44e61c8ae1d7a07780f92a4346d79bbc
Ali <connexion.founder@outlook.com>
2018-03-25 00:35:30 +0800

    Update permission

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { IUser } from '@app/definitions';
 import { RequestsService } from '@services/requests.service';
+import { PermissionsService } from '@app/services/permissions.service';
 
 @Injectable()
 export class UserService {
@@ -9,6 +10,7 @@ export class UserService {
   private user: IUser;
   constructor (
     private requests: RequestsService,
+    private permissions: PermissionsService
   ) {
     try {
       this.user = JSON.parse(localStorage.getItem('userInformation'));
@@ -24,9 +26,25 @@ export class UserService {
     this.user = null;
   }
   canActivate(permissions: Array<string>) {
+    const user = this.User;
+    if (!user.role) {
+      user.role = {
+        permissions: [
+          this.permissions.findByKey('DEVICES::VIEW'),
+          this.permissions.findByKey('WIDGETS::VIEW'),
+          this.permissions.findByKey('LOCATIONS::VIEW'),
+          this.permissions.findByKey('ACTIVITIES::VIEW'),
+          this.permissions.findByKey('ROLES::VIEW'),
+          this.permissions.findByKey('USERS::VIEW')
+        ],
+        id: null,
+        title: 'normal'
+      };
+    }
     if (permissions && permissions.length) {
+
       for (const key of permissions) {
-        const perm = this.User.role.permissions.find(x => x && x.key === key);
+        const perm = user.role.permissions.find(x => x && x.key === key);
         if ( ! perm ) {
           return false;
         }
