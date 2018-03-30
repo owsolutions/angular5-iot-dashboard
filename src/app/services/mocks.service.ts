7e93dcd75dce7616673fd30c2c0f2c992e694e67
Ali <connexion.founder@outlook.com>
2018-03-30 15:02:24 +0700

    Update the devices

--- src/app/services/mocks.service.ts
+++ src/app/services/mocks.service.ts
@@ -22,7 +22,9 @@ export class MockService {
     'GET /api/devices': 'getDevices',
     'POST /api/device': 'postDevice',
     'POST /api/location': 'postLocation',
-    'DELETE /api/location/:id': 'deleteLocation'
+    'DELETE /api/location/:id': 'deleteLocation',
+    'DELETE /api/device/:id': 'deleteDevice'
+
   };
 
   constructor (
@@ -330,6 +332,17 @@ export class MockService {
   }
 
   public deleteLocation (req: HttpRequest<any>): IResponse<any> {
+    return {
+      data: {
+        items: [
+          {
+
+          }
+        ]
+      }
+    };
+  }
+  public deleteDevice (req: HttpRequest<any>): IResponse<any> {
     return {
       data: {
         items: [
