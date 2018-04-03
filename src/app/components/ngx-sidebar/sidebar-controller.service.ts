0d65c5f5d946d3267fef03c6af5b63d6d6c48569
anonymouse <anonymouse>
2018-04-03 12:01:43 +0430

    Fix linting

--- src/app/components/ngx-sidebar/sidebar-controller.service.ts
+++ src/app/components/ngx-sidebar/sidebar-controller.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, EventEmitter, HostListener } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { PagePointerPosition } from '../../definitions';
 
 @Injectable()
