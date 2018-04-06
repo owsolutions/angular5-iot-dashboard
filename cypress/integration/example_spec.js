39c05be1df31f4bf9d40bc1c16250a9d144a2c87
anonymouse <anonymouse>
2018-04-06 10:31:13 +0430

    Write test to signup and login into dashboard

--- cypress/integration/example_spec.js
+++ cypress/integration/example_spec.js
@@ -1,10 +1,26 @@
 describe('Kitchen Sink', function () {
   it('.should() - assert that <title> is correct', function () {
     // https://on.cypress.io/visit
-    cy.visit('http://localhost:4200')
-    cy.get('.ngx-menu-link-device').click();
+    cy.visit('http://localhost:4200');
+
+
+    cy.get('.login-header span').should(($el) => {
+      expect($el).to.contain('Welcome');
+    });
+
+    cy.get('.app-signup-button').click();
+
     cy.wait(100);
-    cy.get('.ngx-menu-link-device-create').click();
+    cy.get('.app-signup-form-email').type('test@test.com');
+    cy.get('.app-signup-form-password').type('123321');
+
+    cy.wait(100);
+    cy.get('.app-signup-form-submit').click();
+
+
+    // cy.get('.ngx-menu-link-device').click();
+    // cy.wait(100);
+    // cy.get('.ngx-menu-link-device-create').click();
   })
 
    
