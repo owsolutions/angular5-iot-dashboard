313c0114e0ead09ac0d1eb63457422eed1350a7c
anonymouse <anonymouse>
2018-04-12 14:53:08 +0430

    Update the code

--- src/app/components/contact-details/contact-details.component.ts
+++ src/app/components/contact-details/contact-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IContact } from '@app/definitions';
 
 @Component({
   selector: 'app-contact-details',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactDetailsComponent implements OnInit {
 
+  public contacts: Array<IContact> = [];
   constructor() { }
 
   ngOnInit() {
+    this.contacts.push({
+      type: 'call',
+      value: ''
+    });
   }
 
 }
