68023593956e1d9c9f37838e88fc8ad482f7a1c3
anonymouse <anonymouse>
2018-04-12 17:53:46 +0430

    Update the code

--- src/app/components/contact-details/contact-details.component.ts
+++ src/app/components/contact-details/contact-details.component.ts
@@ -7,7 +7,7 @@ import { IContact } from '@app/definitions';
   styleUrls: ['./contact-details.component.scss']
 })
 export class ContactDetailsComponent implements OnInit {
-
+  public isRequesting = false;
   public contacts: Array<IContact> = [];
   constructor() { }
 
@@ -17,5 +17,25 @@ export class ContactDetailsComponent implements OnInit {
       value: ''
     });
   }
+  public GetPlaceHolder (type: any) {
+    if (type === 'email') {
+      return 'Please type a valid email';
+    }
+    if ( type === 'call' || type === 'sms') {
+      return 'Please type a internationally reachable phone number';
+    }
+  }
+  public AddAnother () {
+    this.contacts.push({
+      type: 'call',
+      value: ''
+    });
+  }
 
+  public RemoveByIndex (id: number) {
+    this.contacts.splice(id, 1);
+  }
+  public SubmitForm () {
+    this.isRequesting = true;
+  }
 }
