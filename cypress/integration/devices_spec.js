f87ceb87b9955c057fc8c25b84410beeda3b12f5
anonymouse <anonymouse>
2018-04-17 06:47:55 +0430

    Update the test cases

--- cypress/integration/devices_spec.js
+++ cypress/integration/devices_spec.js
@@ -1,9 +1,8 @@
-describe('E2E tests', function () {
+describe('Devices (CloudDevices)', function () {
   beforeEach(() => {
     cy.visit('http://localhost:4200');
     cy.get('.app-login-form-email').type('test@test.com');
     cy.get('.app-login-form-password').type('123321');
-
     cy.wait(100);
     cy.get('.app-login-form-submit').click();
   });
@@ -12,7 +11,7 @@ describe('E2E tests', function () {
     cy.get('.ngx-menu-link-logout').click();
   });
   
-  it('It must be able to create a new device by feeling the form', function () {
+  it('As a user, I must be able to create a new device, and connect to datasource', function () {
     cy.get('.ngx-menu-link-device').click();
     cy.wait(100);
     cy.get('.ngx-menu-link-device-create').click();
@@ -26,11 +25,9 @@ describe('E2E tests', function () {
     cy.wait(100);
   });
 
-  it('We must able to see devices list and created device as well.', function () {
+  it('As a user, I must be able to view list of all devices that I have in my dashboard', function () {
     cy.get('.ngx-menu-link-device').click();
     cy.wait(100);
     cy.get('.ngx-menu-link-device-list').click();
-
-    // cy.get('.app-devices-table').find('tr').should('have.length', 10);
   });
 });
