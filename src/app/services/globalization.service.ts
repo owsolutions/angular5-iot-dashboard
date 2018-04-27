b09e59a3c91b2a75fffd070a804bdd40b8867292
me-is-you <connexion.founder@outlook.com>
2018-04-27 19:27:11 +0200

    Use experimental languages for none-production

--- src/app/services/globalization.service.ts
+++ src/app/services/globalization.service.ts
@@ -1,10 +1,23 @@
 import { Injectable, EventEmitter } from '@angular/core';
+import { environment } from 'environments/environment';
+import { merge } from '@lodash';
+/**
+ * At the moment, we are not supporting other languages rather than english in production wise.
+ * Since this repository is a dependency for commerical version, we will remove the other experimental
+ * languages that community is working on.
+ * If you are building your own version of this repository, feel free to enable them. In case you can
+ * make more contributions to the dashboard, we would appreciate it so please review github repository
+ */
 
-export const SupportedLanguages = {
-  'en': {'title': 'English (United States)', 'direction': 'ltr'},
+export const ExperimentalLanguages = {
   'pl': {'title': 'Polish (Poland)', 'direction': 'ltr'},
   'fa': {'title': 'Persian (فارسی ایران)', 'direction': 'rtl'},
 };
+export const StableLanguages = {
+  'en': {'title': 'English (United States)', 'direction': 'ltr'},
+};
+
+export const SupportedLanguages = environment.production ? StableLanguages : merge(StableLanguages, ExperimentalLanguages);
 @Injectable()
 export class GlobalizationService {
   public layoutDirectionEmmiter: EventEmitter<any> = new EventEmitter();
