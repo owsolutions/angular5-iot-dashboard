9fe48db50026625526791127576a8611d8196277
ali <connexion.founder@outlook.com>
2018-10-28 19:37:28 +0100

    Handle if SimpleBar does not exists

--- src/app/ng5-basic/actions.service.ts
+++ src/app/ng5-basic/actions.service.ts
@@ -4,7 +4,11 @@ declare var SimpleBar: any;
 @Injectable()
 export class ActionsService {
   public scrollTop () {
-    const el = new SimpleBar(document.getElementById('layout'));
-    el.scrollContentEl.scrollTop = 0;
+    try {
+      const el = new SimpleBar(document.getElementById('layout'));
+      el.scrollContentEl.scrollTop = 0;
+    } catch (error) {
+      // intentionally left blank
+    }
   }
 }
